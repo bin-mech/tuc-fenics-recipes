6773da387535ec5a7c8802b43d1fa5f4fb573e96
Jan Blechta <blechta@karlin.mff.cuni.cz>
2018-06-30 19:52:31 +0200

    Don't build PETSc Fortran bindings
    Prevents intenal compiler error in SLEPc Fortran with GNU Fortran
    (Ubuntu 7.2.0-8ubuntu3.2) 7.2.0

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ EIGEN_VERSION=3.3.4
 PYBIND11_VERSION=2.2.3
 PETSC_VERSION=3.9.1
 SLEPC_VERSION=3.9.1
-MPI4PY_VERSION=3.0.0
+#MPI4PY_VERSION=3.0.0
 PETSC4PY_VERSION=3.9.1
 SLEPC4PY_VERSION=3.9.0
 DOLFIN_VERSION="${FENICS_VERSION}.post1"
@@ -24,7 +24,7 @@ mkdir -p "${FENICS_PREFIX}/src"
 pip3 install --user pip
 
 # Install FEniCS Python packages
-pip3 install --prefix="${FENICS_PREFIX}" fenics-ffc==${FENICS_VERSION}
+pip3 install -vv --prefix="${FENICS_PREFIX}" fenics-ffc==${FENICS_VERSION}
 
 # Install Eigen headers
 cd "${FENICS_PREFIX}/src"
@@ -53,27 +53,27 @@ wget -O petsc-${PETSC_VERSION}.tar.gz http://bitbucket.org/petsc/petsc/get/v${PE
 mkdir petsc-${PETSC_VERSION}
 cd petsc-${PETSC_VERSION}
 tar --strip-components=1 -xzf ../petsc-${PETSC_VERSION}.tar.gz
-./configure --with-debugging=0 --COPTFLAGS="-O2" --CXXOPTFLAGS="-O2" --FOPTFLAGS="-O2" --download-metis --download-parmetis --download-ptscotch --download-suitesparse --download-openblas --download-scalapack --download-mumps --download-superlu --download-superlu_dist --download-hypre --download-hdf5 --download-hdf5-configure-arguments="--enable-parallel" --prefix="${FENICS_PREFIX}" --with-make-np=${PROCS}
+./configure --with-debugging=0 --with-fortran-bindings=0 --COPTFLAGS="-O2" --CXXOPTFLAGS="-O2" --FOPTFLAGS="-O2" --download-metis --download-parmetis --download-ptscotch --download-suitesparse --download-openblas --download-scalapack --download-mumps --download-superlu --download-superlu_dist --download-hypre --download-hdf5 --download-hdf5-configure-arguments="--enable-parallel" --prefix="${FENICS_PREFIX}" --with-make-np=${PROCS}
 make
 make install
 export PETSC_DIR="${FENICS_PREFIX}"
 
 # Install SLEPc
-#cd "${FENICS_PREFIX}/src"
-#wget -O slepc-${SLEPC_VERSION}.tar.gz http://bitbucket.org/slepc/slepc/get/v${SLEPC_VERSION}.tar.gz
-#mkdir slepc-${SLEPC_VERSION}
-#cd slepc-${SLEPC_VERSION}
-#tar --strip-components=1 -xzf ../slepc-${SLEPC_VERSION}.tar.gz
-#./configure --prefix="${FENICS_PREFIX}"
-#make -j ${PROCS}
-#make install
-#export SLEPC_DIR="${FENICS_PREFIX}"
+cd "${FENICS_PREFIX}/src"
+wget -O slepc-${SLEPC_VERSION}.tar.gz http://bitbucket.org/slepc/slepc/get/v${SLEPC_VERSION}.tar.gz
+mkdir slepc-${SLEPC_VERSION}
+cd slepc-${SLEPC_VERSION}
+tar --strip-components=1 -xzf ../slepc-${SLEPC_VERSION}.tar.gz
+./configure --prefix="${FENICS_PREFIX}"
+make -j ${PROCS}
+make install
+export SLEPC_DIR="${FENICS_PREFIX}"
 
 # Install various Python packages
-pip3 install --upgrade --prefix="${FENICS_PREFIX}" --ignore-installed numpy jupyter matplotlib sympy pkgconfig
-#pip3 install --prefix="${FENICS_PREFIX}" https://bitbucket.org/mpi4py/mpi4py/downloads/mpi4py-${MPI4PY_VERSION}.tar.gz
-pip3 install --prefix="${FENICS_PREFIX}" https://bitbucket.org/petsc/petsc4py/downloads/petsc4py-${PETSC4PY_VERSION}.tar.gz
-#pip3 install --prefix="${FENICS_PREFIX}" https://bitbucket.org/slepc/slepc4py/downloads/slepc4py-${SLEPC4PY_VERSION}.tar.gz
+pip3 install -vv --upgrade --prefix="${FENICS_PREFIX}" --ignore-installed numpy jupyter matplotlib sympy pkgconfig
+#pip3 install -vv --prefix="${FENICS_PREFIX}" https://bitbucket.org/mpi4py/mpi4py/downloads/mpi4py-${MPI4PY_VERSION}.tar.gz
+pip3 install -vv --prefix="${FENICS_PREFIX}" https://bitbucket.org/petsc/petsc4py/downloads/petsc4py-${PETSC4PY_VERSION}.tar.gz
+pip3 install -vv --prefix="${FENICS_PREFIX}" https://bitbucket.org/slepc/slepc4py/downloads/slepc4py-${SLEPC4PY_VERSION}.tar.gz
 
 # Get DOLFIN (post release)
 cd "${FENICS_PREFIX}/src"
@@ -89,7 +89,7 @@ make install
 
 # Build dolfin Python library
 cd "${FENICS_PREFIX}/src/dolfin/python"
-pip3 install --prefix="${FENICS_PREFIX}" .
+pip3 install -vv --prefix="${FENICS_PREFIX}" .
 
 # Get mshr
 cd "${FENICS_PREFIX}/src"
@@ -105,4 +105,4 @@ make install
 
 # Build mshr Python library
 cd "${FENICS_PREFIX}/src/mshr/python"
-pip3 install --prefix="${FENICS_PREFIX}" .
+pip3 install -vv --prefix="${FENICS_PREFIX}" .
